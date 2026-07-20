69c30cc092e4726217470b3584e09b228bd2a303
CK <chuck.jh.kim@gmail.com>
2026-07-20 03:23:18 +0000

    completed day19

--- scripts/day19.py
+++ scripts/day19.py
@@ -11,7 +11,7 @@ class Device:
 
     def show_info(self):
         # hostname과 ip를 출력하세요.
-        pass
+        print(f"{self.hostname}: {self.ip}")
 
 
 # 2.
@@ -23,7 +23,8 @@ leaf2 = Device("Leaf2", "10.1.1.2")
 
 # 3.
 # 각 객체에서 show_info()를 호출하세요.
-
+leaf1.show_info()
+leaf2.show_info()
 
 # 4.
 # 아래 메서드를 추가하세요.
@@ -35,17 +36,24 @@ class Device:
         self.ip = ip
 
     def show_info(self):
-        pass
+        print(f"{self.hostname}: {self.ip}")
 
     def change_ip(self, new_ip):
         # self.ip를 new_ip로 변경하세요.
-        pass
+        self.ip = new_ip
+
+
+leaf1 = Device("Leaf1", "10.1.1.1")
+leaf2 = Device("Leaf2", "10.1.1.2")
 
 
 # 5.
 # leaf1의 IP를 10.1.1.100으로 변경한 뒤,
 # show_info()를 다시 호출하여 변경되었는지 확인하세요.
 
+leaf1.change_ip("10.1.1.100")
+
+leaf1.show_info()
 
 # GitHub
 
