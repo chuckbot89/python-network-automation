800ae9473d5c176b348d459ec8b5bc2f284347db
CK <chuck.jh.kim@gmail.com>
2026-07-23 19:54:05 +0000

    created feature/day23-super

---
+++ scripts/day23.py
@@ -0,0 +1,58 @@
+# Day23 Assignment - super()
+
+
+class Device:
+    def __init__(self, hostname):
+        self.hostname = hostname
+
+    def show_info(self):
+        print(f"Hostname: {self.hostname}")
+
+
+# 1.
+# Device를 상속받는 CiscoDevice 클래스를 만드세요.
+
+# 2.
+# CiscoDevice의 __init__()에서
+# super().__init__(hostname)을 호출하세요.
+
+# 그리고 self.version을 추가하세요.
+
+
+# 3.
+# show_info()를 오버라이딩하세요.
+
+# 먼저 부모의 show_info()를 실행한 후
+
+# Version: {self.version}
+
+# 을 출력하세요.
+
+
+# 4.
+# 객체를 생성하세요.
+
+# hostname = "Leaf1"
+# version = "10.3"
+
+
+# 5.
+# show_info()를 호출하세요.
+
+
+# 6. (생각 문제)
+
+# super().__init__(hostname)
+
+# 은 새로운 Device 객체를 만드는 것일까요?
+
+# 아니라면 무엇을 하는 것일까요?
+
+
+# GitHub
+
+# Branch
+# feature/day23-super
+
+# Commit
+# feat: use super in inheritance
